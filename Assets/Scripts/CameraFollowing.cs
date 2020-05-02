fa0b7ab186067b06052a44965b177180144101c7
Silvian73 <yurykuz65@gmail.com>
2020-05-02 16:19:47 +0300

    Добавил слежение камеры за игроком

---
+++ Assets/Scripts/CameraFollowing.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class CameraFollowing : MonoBehaviour
+{
+    [SerializeField] private Transform _followedObject;
+    [SerializeField] private float _offset;
+
+    private void LateUpdate()
+    {
+        transform.position = new Vector3(_followedObject.position.x + _offset, transform.position.y, transform.position.z);
+    }
+}
