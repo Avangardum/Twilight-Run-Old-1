cc90476ed8c1e5568cc59c7f20a4e53c1a36fa62
Silvian73 <yurykuz65@gmail.com>
2020-05-03 14:58:35 +0300

    Добавил шипы

---
+++ Assets/Scripts/Spike.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class Spike : MonoBehaviour
+{
+    public enum SpikeColour
+    {
+        White,
+        Black,
+        Red
+    }
+
+    [SerializeField] private SpikeColour _colour;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag(TagManager.GetTagName(TagManager.Tag.PlayerLight)))
+        {
+            if (_colour == SpikeColour.Black || _colour == SpikeColour.Red)
+                KillPlayer();
+        }
+        else if(collision.CompareTag(TagManager.GetTagName(TagManager.Tag.PlayerDark)))
+        {
+            if (_colour == SpikeColour.White || _colour == SpikeColour.Red)
+                KillPlayer();
+        }
+    }
+
+    private void KillPlayer()
+    {
+        Debug.Log("KILL");
+    }
+}
---
+++ Assets/Scripts/TagManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+public class TagManager
+{
+    public enum Tag
+    {
+        PlayerLight,
+        PlayerDark
+    }
+
+    private static Dictionary<Tag, string> _tagNames;
+
+    static TagManager()
+    {
+        _tagNames = new Dictionary<Tag, string>();
+        _tagNames.Add(Tag.PlayerLight, "Player Light");
+        _tagNames.Add(Tag.PlayerDark, "Player Dark");
+    }
+
+    public static string GetTagName(Tag tag) => _tagNames[tag];
+}
