4b7a7739ace6b8580c4eb5ff35900443e5b99ad9
Silvian73 <yurykuz65@gmail.com>
2020-05-05 21:39:31 +0300

    Изменил скрипт камеры, чтобы он работал нормально с разным разрешением

--- Assets/Scripts/CameraFollowing.cs
+++ Assets/Scripts/CameraFollowing.cs
@@ -4,12 +4,22 @@ namespace TwilightRun
 {
     public class CameraFollowing : MonoBehaviour
     {
-        [SerializeField] private Transform _followedObject;
-        [SerializeField] private float _offset;
+        [SerializeField] private Transform _player;
+        [Range(0, 1)][SerializeField] private float _playerXPositionOnCameraPercentage;
+
+        private float _xOffset;
+
+        private void Awake()
+        {
+            float _desiredPlayerScreenXPosition = Screen.width * _playerXPositionOnCameraPercentage;
+            float _desiredPlayerWorldXPosition = Camera.main.ScreenToWorldPoint(new Vector3(_desiredPlayerScreenXPosition, 0, 0)).x;
+            transform.Translate(_player.transform.position.x - _desiredPlayerWorldXPosition, 0, 0);
+            _xOffset = Camera.main.transform.position.x - _player.position.x;
+        }
 
         private void LateUpdate()
         {
-            transform.position = new Vector3(_followedObject.position.x + _offset, transform.position.y, transform.position.z);
+            transform.position = new Vector3(_player.position.x + _xOffset, transform.position.y, transform.position.z);
         }
     }
 
