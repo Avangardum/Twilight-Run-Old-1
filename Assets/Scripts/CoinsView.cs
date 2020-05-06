3d13c89ef006e8b388fe85ec11f262d04fbc132c
Silvian73 <yurykuz65@gmail.com>
2020-05-06 19:27:42 +0300

    Сделал главное меню

---
+++ Assets/Scripts/CoinsView.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using TMPro;
+using TwilightRun;
+
+public class CoinsView : MonoBehaviour
+{
+    private TextMeshProUGUI _text;
+
+    private void Awake()
+    {
+        _text = GetComponent<TextMeshProUGUI>();
+    }
+
+    private void Update()
+    {
+        _text.text = SaveDataManager.Instance.Coins.ToString();
+    }
+}
