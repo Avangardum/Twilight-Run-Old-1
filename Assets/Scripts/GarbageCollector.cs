dfc2e54840b61274d8df153a68743896c1749b5d
Silvian73 <yurykuz65@gmail.com>
2020-05-08 13:39:12 +0300

    Сделал сборщик мусора, убирающий оставленные позади шипы

---
+++ Assets/Scripts/GarbageCollector.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TwilightRun
+{
+    public class GarbageCollector : MonoBehaviour
+    {
+        [SerializeField] private Transform _player;
+        [SerializeField] private float _distanceFromPlayerForDeletion;
+        [SerializeField] private int _clearEveryXFrames;
+
+        private void Clear()
+        {
+            ClearSpikes();
+        }
+
+        private void ClearSpikes()
+        {
+            FindObjectsOfType<Spike>()
+                .Select(x => x.gameObject)
+                .Where(x => x.transform.position.x + _distanceFromPlayerForDeletion < _player.position.x)
+                .ForEach(Destroy);
+        }
+
+        private void Update()
+        {
+            if (Time.frameCount % _clearEveryXFrames == 0)
+                Clear();
+        }
+    } 
+}
---
+++ Assets/Scripts/TwilightRunExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class TwilightRunExtensions
+{
+    public static void ThrowIfNull(this object obj, string name)
+    {
+        if (obj == null) throw new ArgumentException($"Argument {name} is null");
+    }
+
+    public static void ForEach<T>(this IEnumerable<T> source, Action<T> action)
+    {
+        source.ThrowIfNull("source");
+        action.ThrowIfNull("action");
+        foreach (T element in source)
+        {
+            action(element);
+        }
+    }
+}
